fae4a1daf3450e7b9e4bb09997fe52ab66063558
Jonathan Meiss <jonathan@meiss.fr>
2013-08-13 00:02:06 +0200

    Working reels with nice animations but pictures are missing

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,55 +2,74 @@
 
 angular.module('lazyBarista')
   .controller('MainCtrl', function ($scope, $timeout) {
-    var reelsOptions  = [
+    var allRandomizedReelPositions
+    var STATUS = {
+      LOOSE:   -1,
+      PENDING:  0,
+      WIN:      1
+    }
+    var reelsOptions = [
       // COFFEE            TEA             ESPRESSO
       ['Coffe Maker',     'Teapot',       'Espresso Machine'],
       ['Coffee Filter',   'Tea Strainer', 'Espresso Tamper'],
       ['Coffee Grounds',  'Loose Tea',    'Ground Espresso Beans']
-    ];
-
+    ]
+    
+    $scope.isButtonDisabled = false
+    
     $scope.generateReels = function () {
+      randomizeReels()
+      $scope.isButtonDisabled = true
+      $timeout(getResult, 3100)
+    }
+
+
+    var randomizeReels = function () {      
       var i
       var reelLength
       var currentRandomizedReelPosition
-      $scope.hasWon       = undefined
-      $scope.reelsValues  = []
+      allRandomizedReelPositions  = []
+      $scope.resultStatus         = STATUS.PENDING
+      $scope.reelsValues          = []
 
       // Iterate through all reels
       angular.forEach(reelsOptions, function (reel, reelPos) {
+        var nbOfTiles               = 9
         reelLength                  = reel.length
         $scope.reelsValues[reelPos] = []
 
-        // Iterate 10 times to fill reels
-        for (i = 0; i < 10; i++) {
+        // Iterate 'nbOfTiles' times to fill reels
+        for (i = 0; i < nbOfTiles; i++) {
           // Randomize the position for the current reel
           currentRandomizedReelPosition = Math.floor(Math.random() * reel.length)
 
-          // Randomize the position for the current reel
+          // Set the result for the current reel
           $scope.reelsValues[reelPos][i] = reel[currentRandomizedReelPosition]
+
+          // Because reel will always stop at the last tile
+          // Oops... I gave the solution and there is not anymore magie :s 
+          // We keep the last position of the current reel to compare it with other reels
+          if (i === 0) {
+            allRandomizedReelPositions.push(currentRandomizedReelPosition)            
+          }
         }
       })
-
-      $timeout(randomizeReels, 8100)
     }
 
-    var randomizeReels = function () {
-      $scope.hasWon = true
-  
-      // Iterate through all randomized reels positions
-      angular.forEach($scope.reelsValues, function (reelValues) {
-        var firstReelLength   = $scope.reelsValues[0].length - 1
-        var currentReelLength = reelValues.length - 1
-        console.log($scope.reelsValues[0][firstReelLength])
-        console.log(reelValues[currentReelLength])
-        console.log('===')
+    var getResult = function () {
+      var allRandomizedReelPositionsLength  = allRandomizedReelPositions.length
+      $scope.isButtonDisabled               = false
+      $scope.resultStatus                   = STATUS.WIN
 
-        // If last value of first wheel is not equal to last item of current reel
-        // it means all reels positions are not equal so the lazy barista loose
-        // and has to get up and make its drink by himself!
-        if ($scope.reelsValues[0][firstReelLength] !== reelValues[currentReelLength]) {
-          $scope.hasWon = false
+      // Iterate through all randomized reels positions
+      for (var i = 0; i < allRandomizedReelPositionsLength; i++) {
+        // If first value of allRandomizedReelPositions is not equal to one of all
+        // reelPosition it means all reels positions are not equal so the lazy
+        // barista loose and has to get up and make its drink by himself!
+        if (allRandomizedReelPositions[0] !== allRandomizedReelPositions[i]) {
+          $scope.resultStatus = STATUS.LOOSE
+          break;
         }
-      })
+      }
     }
   })
